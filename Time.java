811ea842b54c61579d79b8694216574d92c7f978
aphil311 <41242437+aphil311@users.noreply.github.com>
2018-10-08 20:12:35 -0500

    Classes

---
+++ Time.java
@@ -0,0 +1,27 @@
+public class Time {
+	private char units;
+	private int totalTime;
+	private int totalDistance;
+	public Time(int min, int sec, int distance){
+		totalTime = min*60+sec;
+		if(distance < 4){
+			if(units == 'i'){
+				
+			}
+			else
+				System.out.print("");
+		}
+	}
+	public int getTime(){
+		return totalTime;
+	}
+	public void pushUnits(char iOrE){
+		// Miles = i, Meters = e;
+		if((iOrE == 'i')||(iOrE =='e'))
+			units = iOrE;
+		else
+			System.out.println("Error: Not a valid character");
+		
+	}
+}
+
