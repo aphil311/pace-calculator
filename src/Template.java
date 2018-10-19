0358c6769d4428e612f63ac602a8f31e049ea84a
Aidan Phillips <APhillips@dhs.local>
2018-10-19 10:38:45 -0500

    new class to handle large blocks of system out commands

---
+++ src/Template.java
@@ -0,0 +1,21 @@
+public class Template{
+  public static void distanceMenu(){
+    System.out.println("-------------");
+		System.out.println("| 1. 1600m  |");
+		System.out.println("| 2. 3200m  |");
+		System.out.println("| 3. 5000m  |");
+		System.out.println("| 4. 1 Mile |");
+		System.out.println("| 5. 2 Mile |");
+		System.out.println("| 6. 3 Mile |");
+		System.out.println("| 7. Half   |");
+		System.out.println("| 8. Full   |");
+		System.out.println("| 9. Custom |");
+		System.out.println("-------------");
+  }
+  public static void unitMenu(){
+    System.out.println("-------------");
+    System.out.println("| 1. Meters |");
+    System.out.println("| 2. Miles  |");
+    System.out.println("-------------");
+  }
+}
